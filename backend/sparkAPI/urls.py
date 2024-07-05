d7684ccb934031c786c678354b88f9a49b49a318
Ftre <1169509174@qq.com>
2024-07-05 14:06:13 +0800

    后端更新

--- backend/sparkAPI/urls.py
+++ backend/sparkAPI/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('generate_repo_json/', views.generate_target_repos_json, name='create_repo_json'),
-    path('view_repo_json/', views.view_target_repos_json, name='view_repo_json'),
+    # path('generate_repo_json/', views.generate_target_repos_json, name='create_repo_json'),
+    # path('view_repo_json/', views.view_target_repos_json, name='view_repo_json'),
     path('generate_repo_json_secure/', views.generate_target_repos_json_secure, name='create_repo_json_secure'),
     path('view_repo_json_secure/', views.view_target_repos_json_secure, name='view_repo_json_secure')
 ]
