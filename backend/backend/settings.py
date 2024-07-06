463e86c2473bc2519b24fe21d1820465e67f7c2c
passer12 <93042040+passer12@users.noreply.github.com>
2024-07-06 14:48:14 +0800

    修改配置之前保存一下

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', 'backend']
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
